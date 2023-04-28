cbb30787f6d2f18fa0b8a97d0650b6907c0f7afe
AxelCh14 <afcs.web.creative@gmail.com>
2023-04-28 03:50:47 -0600

    changes in API

--- src/data/httpClient.js
+++ src/data/httpClient.js
@@ -1,20 +1,20 @@
-  const API = 'https://jsonplaceholder.typicode.com/users';
-  const IMG_API = 'https://joesch.moe/api/v1/random';
+const API = 'https://jsonplaceholder.typicode.com/users';
+const IMG_API = 'https://joesch.moe/api/v1/random';
 
-  export async function get() {
-    try {
-      const response = await fetch(API);
-      const users = await response.json();
-      const usersWithAvatars = users.map((user) => ({
-        id: user.id,
-        name: user.name,
-        phone: user.phone,
-        email: user.email,
-        avatar: `${IMG_API}?=${user.id}`,
-      }));
-      return usersWithAvatars;
-    } catch (error) {
-      console.error(error);
-      throw new Error('Error fetching data from server');
-    }
+export async function get() {
+  try {
+    const response = await fetch(API);
+    const users = await response.json();
+    const usersWithAvatars = users.map((user) => ({
+      id: user.id,
+      name: user.name,
+      phone: user.phone,
+      email: user.email,
+      avatar: `${IMG_API}?=${user.id}`,
+    }));
+    return usersWithAvatars;
+  } catch (error) {
+    console.error(error);
+    throw new Error('Error fetching data from server');
   }
+}
