73cf7927a4110d3bbb32fada04f527976f17caea
AxelCh14 <afcs.web.creative@gmail.com>
2023-04-28 21:59:16 -0600

    set menu hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import MenuIcon from "@mui/icons-material/Menu";
-import CloseIcon from "@mui/icons-material/Close";
-import Button from "@mui/material/Button";
 import "./App.css";
 import { MyRoutes } from "./routers/routes";
 
@@ -20,28 +18,16 @@ function App() {
           <MenuIcon
             className="menu-icon"
             onClick={toggleMenu}
-            style={{ display: showMenu ? "none" : "block" }}
           />
-          <span className="logo-text" style={{ display: showMenu ? "none" : "block" }}>
-            Mi aplicación
-          </span>
+          <span className="logo-text"></span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto" style={{ display: showMenu ? "block" : "none" }}>
-            <Nav.Link href="#home">Inicio</Nav.Link>
             <Nav.Link href="#link">Página 1</Nav.Link>
             <Nav.Link href="#link">Página 2</Nav.Link>
             <Nav.Link href="#link">Página 3</Nav.Link>
           </Nav>
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={toggleMenu}
-            style={{ display: showMenu ? "block" : "none" }}
-          >
-            <CloseIcon />
-          </Button>
         </Navbar.Collapse>
       </Navbar>
       <MyRoutes />
