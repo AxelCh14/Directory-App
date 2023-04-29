79b5012dacb8122745cbbf94a629f868c80d1c63
AxelCh14 <afcs.web.creative@gmail.com>
2023-04-28 21:17:24 -0600

    cardstyles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,19 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
+import "../components/Cardstyle.css"
+
 export function Card({ user }) {
   return (
     <Link to={`/Details/${user.id}`} className="card-link">
-      <div className="card">
+      <div className="card contact-card">
         <img
-          className="avatar"
+          className="avatar contact-image"
           src={user.avatar}
           alt={user.name}
           width={200}
           height={200}
         />
-        <div>{user.name}</div>
+        <div className="contact-info">{user.name}</div>
       </div>
     </Link>
   );
