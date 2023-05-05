f9c2e0f1055a379cc3ad7d731a6445bba812b3f3
AxelCh14 <afcs.web.creative@gmail.com>
2023-05-05 16:36:21 -0600

    Corregido avatar name

--- src/utils/getContactImg.js
+++ src/utils/getContactImg.js
@@ -3,5 +3,5 @@ import placeholder from "../img/3973421.png";
 const AVATAR_API = "https://ui-avatars.com/api/";
 
 export function getContactImg(name) {
-  return name ? `${AVATAR_API}?name=${encodeURIComponent(name)}` : placeholder;
+  return name ? `${AVATAR_API}?name=${encodeURIComponent(name)}&size=200` : placeholder;
 }
